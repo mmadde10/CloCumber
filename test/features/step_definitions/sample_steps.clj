4779b432ba6df554d70b726709fa4c9bed82c973
Mark Madden <mark.madden@rate.com>
2018-03-06 18:39:56 -0600

    Gherkin added

--- test/features/step_definitions/sample_steps.clj
+++ test/features/step_definitions/sample_steps.clj
@@ -1,11 +1,19 @@
 (use 'clojure-cukes.core)
 (use 'clojure.test)
+(ns browser (:require [src.clocumber.browser :as browser]))
 
-(Given #"^I have (\d+) big \"([^\"]*)\" in my belly$" [n, thing]
-       (reset! belly (repeat (read-string n) thing)))
 
-(When #"I eat (\d+) \"([^\"]*)\"" [n, thing]
+(Given #"^I have a browser"
+       (context (browser\make_browser))
+
+(When #"I visit \"([^\"]*)\"" [website]
       (eat (repeat (read-string n) thing)))
 
-(Then #"^I am \"([^\"]*)\"$" [mood-name]
-      (assert (= (name (mood)) mood-name)))
+(and #"I fill in \"([^\"]*)\" with \"([^\"]*)\"" [search text]
+      (eat (repeat (read-string n) thing)))
+
+(and #"I press \"([^\"]*)\"" [button]
+      (eat (repeat (read-string n) thing)))
+
+(Then #"^I should see \"([^\"]*)\"" [page-name]
+      (assert (= (name (page)) page-name)))
